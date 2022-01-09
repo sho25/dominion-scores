f193739093023a9a01197165f9691fce3e5dcf7c
Sho Ikeda <ikeda@posl.ait.kyusyu-u.ac.jp>
2022-01-09 14:59:20 +0900

    fix hook dependency

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -13,7 +13,7 @@ export default function useLocalStorage<T>(key: string, defaultValue: T) {
 
   useEffect(() => {
     localStorage.setItem(key, JSON.stringify(value));
-  }, [value]);
+  }, [value, key]);
 
   return [value, setValue];
 }
